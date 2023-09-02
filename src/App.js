062b36733ec6618b5ee65df7f22adeea0f8638cb
gabisile <137975209+Gabisile-felicia@users.noreply.github.com>
2023-09-03 00:22:09 +0200

    installed react-animated-weather icons for  the weather forecast. added css to app.js... added five new components to the app, each to target a specific section on the app, the icon, forecast etc.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 import "bootstrap/dist/css/bootstrap.css";
@@ -6,10 +7,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather />
+        <Weather defaultCity="Mbombela" />
         <small>
           Check this project's{" "}
-          <a href="https://github.com/Gabisile-felicia/weather-react-app">
+          <a
+            href="https://github.com/Gabisile-felicia/weather-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Github Repository
           </a>{" "}
         </small>
--- src/Weather.js
+++ src/Weather.js
@@ -1,70 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
 
-export default function Weather() {
-  let weatherData = {
-    city: "Mbombela",
-    temperature: 27,
-    date: "Sunday 13:00",
-    description: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    feelsLike: 27,
-    humidity: 5,
-    wind: 3,
-  };
-  return (
-    <div className="container">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="text"
-              placeholder="Type a city..."
-              className="form-control shadow-sm"
-              autoComplete="off"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "c5f0e59acac64258bb92ed027d20c68f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="container shadow-sm">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="text"
+                placeholder="Type a city..."
+                className="form-control shadow-sm"
+                autoComplete="off"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="form-control shadow-sm submit"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="form-control shadow-sm submit"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="col-12">
-        <div className="card-body">
-          <h1>Mbombela</h1>
-          <h2>{weatherData.date}</h2>
-          <h3>
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="Clear"
-            />
-            <span className="temperature">{weatherData.temperature}</span>
-            <span>°C</span>
-          </h3>
-        </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
-
-      <div className="row">
-        <div className="col-4">
-          <div className="card-body">{weatherData.description}</div>
-        </div>
-
-        <div className="col-4">
-          <div className="card-body">
-            Humidity <span>{weatherData.humidity}</span>%
-          </div>
-        </div>
-
-        <div className="col-4">
-          <div className="card-body">
-            Wind <span>{weatherData.wind}</span>Km/h
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
